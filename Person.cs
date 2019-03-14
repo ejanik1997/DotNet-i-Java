e2b057f7f7f5a4cc3484cc9d2fcf85ce902da82d
DragonCyclopWizzard <235297@student.pwr.edu.pl>
2019-03-14 01:03:56 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Lab01
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public BitmapImage PersonImage { get; set; }
+    }
+}
